97c607d7acd5e172c9c8220c39aa2da37384668a
Mikel Garín <mikelgarin@gmail.com>
2024-07-25 02:19:30 +0200

    Solved kata

--- src/main/java/com/desierto/PeanoCalculator.java
+++ src/main/java/com/desierto/PeanoCalculator.java
@@ -1,6 +1,12 @@
 package com.desierto;
 
+import static com.desierto.Ordering.EQ;
+import static com.desierto.Ordering.GT;
+import static com.desierto.Ordering.LT;
+
 public class PeanoCalculator {
+
+  private static final Peano ONE = new Succ(Zero.INSTANCE);
 
   public static Peano peano(int n) {
     if (0 == n) return Zero.INSTANCE;
@@ -8,7 +14,24 @@ public class PeanoCalculator {
   }
 
   static Peano add(Peano a, Peano b) {
-    return a;
+    if (a instanceof Zero) {
+      return b;
+    } else if (b instanceof Zero) {
+      return a;
+    } else {
+      Succ aSucc = (Succ) a;
+      return recursiveAddition(aSucc, b);
+    }
+  }
+
+  private static Peano recursiveAddition(Succ a, Peano b) {
+    Succ newA;
+    if (a.peano instanceof Zero) {
+      newA = new Succ(b);
+    } else {
+      newA = new Succ(recursiveAddition((Succ) a.peano, b));
+    }
+    return newA;
   }
 
   /**
@@ -17,32 +40,90 @@ public class PeanoCalculator {
    *
    */
   static Peano sub(Peano a, Peano b) {
-    return a;
+    if (a instanceof Zero) {
+      if (b instanceof Zero) {
+        return Zero.INSTANCE;
+      }
+      throw new ArithmeticException("return value is negative");
+    }
+    if (b instanceof Zero) {
+      return a;
+    }
+
+    return recursiveSubstraction(a, b);
+  }
+
+  private static Peano recursiveSubstraction(Peano a, Peano b) {
+    if (a instanceof Zero) {
+      throw new ArithmeticException("return value is negative");
+    }
+    if (b instanceof Zero) {
+      return a;
+    }
+    return recursiveSubstraction(((Succ)a).peano, ((Succ)b).peano);
   }
 
   static Peano mul(Peano a, Peano b) {
-    return a;
+    if (a instanceof Zero || b instanceof Zero) {
+      return Zero.INSTANCE;
+    }
+    return recursiveMultiplication(a, b);
   }
 
+  private static Peano recursiveMultiplication(Peano a, Peano b) {
+    if (b instanceof Zero) {
+      return Zero.INSTANCE;
+    }
+    return add(recursiveMultiplication(a, ((Succ)b).peano), a);
+  }
   /**
    * @throws ArithmeticException if divided by 0
    *
    *
    */
   static Peano div(Peano a, Peano b) {
-    return a;
+    if (b instanceof Zero) {
+      throw new ArithmeticException("division by 0");
+    }
+    if (a instanceof Zero) {
+      return Zero.INSTANCE;
+    }
+    return recursiveDivision(a, b, Zero.INSTANCE, b);
+  }
+
+  private static Peano recursiveDivision(Peano a, Peano b, Peano accumulative, Peano newDivision) {
+    if (compare(a,newDivision).equals(GT) || compare(a,newDivision).equals(EQ)) {
+      return recursiveDivision(a, b, add(accumulative, ONE), add(newDivision, b));
+    }
+    return accumulative;
   }
 
   static boolean even(Peano peano) {
-    return true;
+    return recursiveParity(peano, true);
   }
 
   static boolean odd(Peano peano) {
-    return true;
+    return recursiveParity(peano, false);
+  }
+
+  private static boolean recursiveParity(Peano peano, boolean parity) {
+    if (peano instanceof Zero) {
+      return parity;
+    }
+    return recursiveParity(((Succ)peano).peano, !parity);
   }
 
   static Ordering compare(Peano a, Peano b) {
-    return Ordering.EQ;
+    if (a instanceof Zero && b instanceof Succ) {
+      return LT;
+    }
+    if (a instanceof Succ && b instanceof Zero) {
+      return GT;
+    }
+    if (a instanceof Zero && b instanceof Zero) {
+      return EQ;
+    }
+    return compare(((Succ) a).peano, ((Succ) b).peano);
   }
 
 }
--- src/test/java/com/desierto/PeanoCalculatorTest.java
+++ src/test/java/com/desierto/PeanoCalculatorTest.java
@@ -13,6 +13,7 @@ import static com.desierto.PeanoCalculator.peano;
 import static com.desierto.PeanoCalculator.sub;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -23,40 +24,60 @@ public class PeanoCalculatorTest {
     assertEquals(EQ, compare(a, b));
   }
 
+  private static void assertPeano(Peano a, Peano b, Ordering ordering) {
+    assertEquals(ordering, compare(a, b));
+  }
+
   @Test
   public void addTest() throws Exception {
     assertPeanoEq(peano(0), add(peano(0), peano(0)));
     assertPeanoEq(peano(664), add(peano(0), peano(664)));
+    assertPeanoEq(peano(5), add(peano(3), peano(2)));
+    assertPeanoEq(peano(761), add(peano(537), peano(224)));
   }
 
   @Test
   public void subTest() throws Exception {
     assertPeanoEq(peano(0), sub(peano(0), peano(0)));
     assertPeanoEq(peano(10), sub(peano(10), peano(0)));
+    assertPeanoEq(peano(1), sub(peano(3), peano(2)));
+    assertPeanoEq(peano(313), sub(peano(537), peano(224)));
+    assertThrows(ArithmeticException.class, () -> sub(peano(0), peano(1)));
+    assertThrows(ArithmeticException.class, () -> sub(peano(5), peano(7)));
   }
 
   @Test
   public void mulTest() throws Exception {
     assertPeanoEq(peano(0), mul(peano(0), peano(0)));
     assertPeanoEq(peano(0), mul(peano(5), peano(0)));
+    assertPeanoEq(peano(35), mul(peano(7), peano(5)));
+    assertPeanoEq(peano(744), mul(peano(31), peano(24)));
   }
 
   @Test
   public void divTest() throws Exception {
     assertPeanoEq(peano(4), div(peano(8), peano(2)));
     assertPeanoEq(peano(3), div(peano(10), peano(3)));
+    assertPeanoEq(peano(7), div(peano(123), peano(16)));
+    assertPeanoEq(peano(0), div(peano(0), peano(16)));
+    assertThrows(ArithmeticException.class, () -> div(peano(5), peano(0)));
+    assertThrows(ArithmeticException.class, () -> div(peano(0), peano(0)));
   }
 
   @Test
   public void evenTest() throws Exception {
     assertTrue(even(peano(0)));
     assertTrue(even(peano(8)));
+    assertFalse(even(peano(57)));
+    assertFalse(even(peano(123)));
   }
 
   @Test
   public void oddTest() throws Exception {
     assertFalse(odd(peano(0)));
     assertFalse(odd(peano(8)));
+    assertTrue(odd(peano(57)));
+    assertTrue(odd(peano(123)));
   }
 
   @Test
